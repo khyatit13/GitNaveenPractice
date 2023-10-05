acd6ba7440e058dad6a1bcbb0027672e9f7863b5
khyati tuwani <40394273+khyatit13@users.noreply.github.com>
2023-10-04 18:02:34 -0700

    Update loginPage.java

--- src/Pages/loginPage.java
+++ src/Pages/loginPage.java
@@ -3,8 +3,8 @@ package Pages;
 public class loginPage {
 	public static void main(String args[]) {
 		System.out.println("Login Page");
-		System.out.println("Checking merge Page");
-		System.out.println("Chechk again");
+		System.out.println("Checking Main Page");
+		System.out.println("Chechk Main");
 		
 	}
 }
